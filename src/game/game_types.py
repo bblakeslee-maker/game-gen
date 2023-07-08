a4de81b6b1b0ce931985b1e5a23604ac843de4d9
Gary Heckman <gheckman@carnegierobotics.com>
2023-07-08 18:35:14 -0400

    Cutscenes use generated text
    Also ignore pycache

--- src/game/game_types.py
+++ src/game/game_types.py
@@ -5,3 +5,6 @@ from .chatbot import StoryTeller
 @dataclasses.dataclass
 class GameState:
     story_teller: StoryTeller
+    window_size: tuple[int, int]
+    is_prologue: bool
+    battle_won: bool
