4e36e07ac773a9fe3a042bd0f035bee1f8bf1fae
Gary Heckman <gheckman92@gmail.com>
2023-07-08 18:43:28 -0400

    Cutscenes use generated text (#13)
    Also ignore pycache

--- src/game/director.py
+++ src/game/director.py
@@ -6,21 +6,21 @@ from .game_types import GameState
 from .chatbot import StoryTeller
 
 
-
 class Director:
     state: GameState
     window: arcade.Window
 
     def __init__(self, window: arcade.Window):
         self.window = window
-        self.state = GameState(story_teller=StoryTeller(use_cache=True))
+        self.state = GameState(story_teller=StoryTeller(use_chatgpt=False), window_size=(800, 600), is_prologue=True, battle_won=False)
 
+        # Should be retrieved from the SetupController
+        self.state.story_teller.add_basic_character_info("Bob", "Builder", "He can totally fix anything, except his marriage.")
 
         # game_flow = [SetupController, CutsceneController, BattleController, CutsceneController]
-        game_flow = [SetupController, CutsceneController, BattleController]
+        game_flow = [SetupController, CutsceneController, BattleController, CutsceneController]
         self.scene_iter = iter(game_flow)
         self.current_scene = None
-
 
     def advance_game_flow(self):
         callback = self.advance_game_flow
