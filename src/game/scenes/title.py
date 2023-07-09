d41727a8b12df238e8c6f571dfc3afba6871f011
Gary Heckman <gheckman@carnegierobotics.com>
2023-07-09 17:21:13 -0400

    Temp fix title

--- src/game/scenes/title.py
+++ src/game/scenes/title.py
@@ -4,6 +4,57 @@ from ..game_types import GameState
 import game.dialog_box as dialog_box
 from game.scenes.cutscene import Background
 
+class Darkness(arcade.Section):
+    def __init__(self, left, bottom, width, height, **kwargs):
+        super().__init__(left, bottom, width, height, **kwargs)
+        self.left = left
+        self.bottom = bottom
+        self.width = width
+        self.height = height
+        self.background = dialog_box.Drawable(color = [0, 0, 0, 160])
+
+    def set_callback(self, callback):
+        self.callback = callback
+
+    def on_draw(self):
+        self.background.draw(self.left, self.bottom, self.width, self.height)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if self.callback != None:
+            self.callback()
+
+class TitleText(arcade.Section):
+    def __init__(self, left, bottom, width, height, **kwargs):
+        super().__init__(left, bottom, width, height, **kwargs)
+        self.content = ""
+
+        self.left = left
+        self.bottom = bottom
+        self.width = width
+        self.height = height
+
+        self.font_size = 50
+        self.padding = 50
+
+    def set_callback(self, callback):
+        self.callback = callback
+
+    def on_draw(self):
+        start_y = self.height // 2 - self.font_size
+
+        arcade.draw_text(
+                self.content,
+                0, start_y,
+                arcade.color.WHITE,
+                self.font_size,
+                multiline=True,
+                width=self.width - 2 * self.padding,
+                align="center")
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        if self.callback != None:
+            self.callback()
+
 class TitleView(arcade.View):
     def __init__(self, state: GameState, is_done_callback):
         super().__init__()
@@ -12,37 +63,48 @@ class TitleView(arcade.View):
         self.width = state.window_size[0]
         self.height = state.window_size[1]
 
-        self.content = self.state.story_teller.title
-
         self.time_elapsed = 0
-        self.done_waiting = True #False
+        self.done_waiting = False
 
         self.bg_section = Background(0,
                                      0,
                                      self.width,
                                      self.height)
 
+        self.darkness_section = Darkness(0,
+                                         0,
+                                         self.width,
+                                         self.height)
+        self.darkness_section.set_callback(lambda: self.dialog_next())
+
+        self.title_section = TitleText(0,
+                                       0,
+                                       self.width,
+                                       self.height)
+
+        self.title_section.set_callback(lambda: self.dialog_next())
+        self.title_section.content = self.state.story_teller.title
+
         background_path = self.state.image_generator.get_background('title-card')
-        print(background_path)
 
         self.bg_section.open(dialog_box.Drawable(texture = arcade.load_texture(background_path)))
         self.section_manager.add_section(self.bg_section)
-
+        self.section_manager.add_section(self.darkness_section)
+        self.section_manager.add_section(self.title_section)
 
     def on_update(self, delta_time: float):
         self.time_elapsed += delta_time
 
-        if self.time_elapsed > 5.0:
+        if self.time_elapsed > 3.0:
             self.done_waiting = True
+            self.done()
 
     def on_draw(self):
-        FONT_SIZE = 50
-
         arcade.start_render()
 
-        # Assume that the title is 2 lines
-        start_y = (self.height - FONT_SIZE) // 2 + FONT_SIZE
-        arcade.draw_text(self.content, 0, start_y, arcade.color.WHITE, FONT_SIZE, width=self.width, align="center")
+    def dialog_next(self):
+        if self.done_waiting:
+            self.done()
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         if self.done_waiting:
