00caadd56328085c2264e2e0166b6fe1a2b3f14b
Anthony Aniszewski <aaniszewski@carnegierobotics.com>
2023-07-10 06:46:32 -0400

    Battle with healing and boss actions

--- src/game/scenes/battle.py
+++ src/game/scenes/battle.py
@@ -5,12 +5,12 @@ from textwrap import wrap as wrap_text
 import random
 import json
 
-PLAYER_SCALING = 1.0
-BOSS_SCALING = 0.9
+PLAYER_SCALING = 0.8
+BOSS_SCALING = 0.8
 
 MENU_HEIGHT = 200
 
-BOSS_MAX_HP = 2600
+BOSS_MAX_HP = 2300
 PLAYER_MAX_HP = 2400
 
 
@@ -50,6 +50,9 @@ class BattleView(arcade.View):
         self.state:GameState = state
         self.width = state.window_size[0]
         self.height = state.window_size[1]
+        self.post_action_phase = False
+        self.post_action_text = "If you see this something has happened out of order"
+        self.time_in_post_action = 0.0
         self.battle_done = False
         self.current_main_action_index = 0  # Initial main action index is 0 (Attack)
         self.current_subaction_index = 0  # Initial subaction index is 0 (first action under Attack)
@@ -57,6 +60,7 @@ class BattleView(arcade.View):
         self.players_turn = True
         self.boss_health = BOSS_MAX_HP
         self.player_health = PLAYER_MAX_HP
+        self.boss_items_left = 2
         self.boss_final_wind_flag = False
         self.player_attacks = json.loads(self.state.story_teller.main_character_attacks)
         self.player_items = json.loads(self.state.story_teller.main_character_inventory)
@@ -103,48 +107,87 @@ class BattleView(arcade.View):
         arcade.draw_lrwh_rectangle_textured(0, 0, self.width, self.height, self.background)
 
         self.player_sprite = arcade.Sprite(player_image, PLAYER_SCALING)
-        self.player_sprite.center_x = 120
-        self.player_sprite.center_y = 320
+        self.player_sprite.center_x = self.width / 6
+        self.player_sprite.center_y = 440
         self.player_sprite.draw()
 
         enemy_image = self.state.image_generator.get_portrait(self.state.story_teller.final_boss_name)
         self.enemy_sprite = arcade.Sprite(enemy_image, BOSS_SCALING)
 
-        self.enemy_sprite.center_x = 750
-        self.enemy_sprite.center_y = 420
+        self.enemy_sprite.center_x = (self.width / 2) + (self.width / 3) 
+        self.enemy_sprite.center_y = 500
         self.enemy_sprite.draw()
 
         self.player_hp_bar.draw()
         self.enemy_hp_bar.draw()
 
         BORDER_THICKNESS = 4  # border thickness in pixels
-        for i in range(3):
-            panel_x = self.width * (i * 2 + 1) / 6  # x position of the panel
-            # Draw the border (larger rectangle)
-            arcade.draw_rectangle_outline(panel_x, MENU_HEIGHT / 2, self.width / 3, MENU_HEIGHT, arcade.color.BLACK,
-                                          BORDER_THICKNESS)
-            # Draw the panel (smaller rectangle)
-            arcade.draw_rectangle_filled(panel_x, MENU_HEIGHT / 2, self.width / 3 - BORDER_THICKNESS,
-                                         MENU_HEIGHT - BORDER_THICKNESS, arcade.color.WHITE)
+        
+        if self.post_action_phase == True:
+            arcade.draw_rectangle_filled(self.width / 2, MENU_HEIGHT / 2, self.width, MENU_HEIGHT, arcade.color.BLACK, 0)
+            # arcade.draw_text(self.post_action_text, self.width / 2, self.height / 2, arcade.color.WHITE, font_size=32, align="center")
+            arcade.draw_text(self.post_action_text, 0, MENU_HEIGHT / 2, arcade.color.WHITE, font_size=20, width=self.width, align="center")
+        else:
+            for i in range(3):
+                panel_x = self.width * (i * 2 + 1) / 6  # x position of the panel
+                # Draw the border (larger rectangle)
+                arcade.draw_rectangle_outline(panel_x, MENU_HEIGHT / 2, self.width / 3, MENU_HEIGHT, arcade.color.BLACK,
+                                            BORDER_THICKNESS)
+                print("current_panel: "+str(self.current_panel)+"i: "+str(i))
+                if self.current_panel == 0 and i != 0:
+                    panel_color = arcade.color.DIM_GRAY
+                elif self.current_panel == 1 and i == 0:
+                    panel_color = arcade.color.DIM_GRAY
+                else:
+                    panel_color = arcade.color.WHITE
+                self.current_subaction_index 
+                # Draw the panel (smaller rectangle)
+                arcade.draw_rectangle_filled(panel_x, MENU_HEIGHT / 2, self.width / 3 - BORDER_THICKNESS,
+                                            MENU_HEIGHT - BORDER_THICKNESS, panel_color)
 
-        # Draw the main action options
-        for i, action in enumerate(self.main_actions):
-            color = arcade.color.BLACK if i == self.current_main_action_index else arcade.color.DIM_GRAY
-            arcade.draw_text(action, self.width / 6 - 100, MENU_HEIGHT - (i + 1) * 30, color, font_size=20)
 
-        # Draw the subaction options for the current main action
-        for i, subaction in enumerate(self.subactions[self.current_main_action]):
-            color = arcade.color.BLACK if i == self.current_subaction_index else arcade.color.DIM_GRAY
-            arcade.draw_text(subaction, self.width / 2 - 100, MENU_HEIGHT - (i + 1) * 30, color, font_size=21)
+            # Draw the main action options
+            for i, action in enumerate(self.main_actions):
+                color = arcade.color.BLACK if i == self.current_main_action_index else arcade.color.DIM_GRAY
+                arcade.draw_text(action, self.width / 6 - 100, MENU_HEIGHT - (i + 1) * 30, color, font_size=20)
 
-        # Draw the description for the current subaction
-        if self.current_subaction in self.actions_descriptions:
-            description = self.actions_descriptions.get(self.current_subaction, "")
-            lines = wrap_text(description, 10)
-            for i, line in enumerate(lines):
-                y = MENU_HEIGHT / 2 + (len(lines) / 2 - i) * 25  # Dynamically adjust y position based on how many lines there are
-                arcade.draw_text(line, self.width * 2 / 3 + 50, y, arcade.color.BLACK, font_size=20,
-                                 width=self.width / 4, align="left")
+            # Draw the subaction options for the current main action
+            for i, subaction in enumerate(self.subactions[self.current_main_action]):
+                color = arcade.color.BLACK if i == self.current_subaction_index else arcade.color.DIM_GRAY
+                arcade.draw_text(subaction, self.width / 2 - 100, MENU_HEIGHT - (i + 1) * 30, color, font_size=21)
+
+            # Draw the description for the current subaction
+            if self.current_subaction in self.actions_descriptions:
+                description = self.actions_descriptions.get(self.current_subaction, "")
+                action_effect_text = ""
+                accuracy_text = ""
+
+                if self.current_main_action_index == 0:
+                    action_effect = int(self.player_attacks[self.current_subaction_index]["damage"])
+                    action_accuracy = str(self.player_attacks[self.current_subaction_index]["accuracy"])
+                    accuracy_text += "Accuraracy: " + action_accuracy + "%"
+                elif self.current_main_action_index == 1:
+                    action_effect = int(self.player_items[self.current_subaction_index]["damage"])
+                
+                if action_effect > 0: 
+                    action_effect_text += "Deals: " + str(action_effect) +  " Damage"
+                else:
+                    action_effect_text += "Heals: " + str(abs(action_effect)) +  "HP"
+
+                lines = wrap_text(description, 32)
+                last_y = 0
+                for i, line in enumerate(lines):
+                    y = MENU_HEIGHT / 2 + (len(lines) / 2 - i) * 25  # Dynamically adjust y position based on how many lines there are
+                    arcade.draw_text(line, self.width * 2 / 3 + 50, y, arcade.color.BLACK, font_size=16,
+                                    width=self.width / 4, align="left")
+                    last_y = y
+                
+          
+                arcade.draw_text(action_effect_text, self.width * 2 / 3 + 50, last_y - 30, arcade.color.BLACK, font_size=16, 
+                                    width=self.width / 4, align="left")
+
+                arcade.draw_text(accuracy_text, self.width * 2 / 3 + 50, last_y - 55, arcade.color.BLACK, font_size=16, 
+                                    width=self.width / 4, align="left")
 
 
 
@@ -177,15 +220,18 @@ class BattleView(arcade.View):
                     damage = int(self.player_attacks[self.current_subaction_index]["damage"])
                     print("Player attacks with: " + self.current_subaction + " for " + str(damage) + " damage")
 
+                if damage > 0:
                     if -200 < (self.boss_health - damage) < 0 and self.boss_final_wind_flag == False:
                         self.boss_health = 400
                         boss_final_wind_flag = True
                         print("Final blow wasn't strong enough boss experiances final wind")
                     else:
                         self.boss_health -= damage
+                else:
+                    self.player_health -= damage
 
-                    print("Boss health is currently " + str(self.boss_health))
-                    self.players_turn = False
+                print("Boss health is currently " + str(self.boss_health))
+                self.players_turn = False
 
 
     @property
@@ -205,30 +251,51 @@ class BattleView(arcade.View):
             self.state.battle_won = True
             self.battle_done = True
             self.done()
-        
 
-        # Boss's Turn
-        if self.players_turn == False:
+        if self.post_action_phase == True:
+            # if self.time_in_post_action >= 0.0:
+            self.time_in_post_action += delta_time
+            if self.time_in_post_action > 1.5:
+                self.time_in_post_action = 0
+                self.post_action_phase = False
+        else:
+            # Boss's Turn
+            if self.players_turn == False:
 
-            # Randomly pick an action
-            enemy_attack_index = random.randint(0, 3)
+                boss_action_text = ""
+                    
+                if self.boss_items_left > 0 and random.randrange(100) < 30: # Should boss use item
+                     # Randomly pick an item
+                    enemy_action_index = random.randint(0, 1)
+                    item_name = self.enemy_attacks[enemy_action_index]["name"]
+                    damage = int(self.enemy_items[enemy_action_index]["damage"])
+                    if damage > 0:
+                        self.player_health -= damage
+                        boss_action_text = self.state.story_teller.final_boss_name + " attacks with item " + item_name + " for " + str(damage) + " damage"
+                    else:
+                        self.boss_health -= damage
+                        boss_action_text = self.state.story_teller.final_boss_name + " heals with item " + item_name + " for " + str(damage) + " HP"
+                    self.boss_items_left -= 1
+                else:
+                    # Randomly pick an attack
+                    enemy_action_index = random.randint(0, 3)
+                    attack_name = self.enemy_attacks[enemy_action_index]["name"]
+                    damage = int(self.enemy_attacks[enemy_action_index]["damage"])
+                    if damage > 0:
+                        self.player_health -= damage
+                        boss_action_text = self.state.story_teller.final_boss_name + " attacks with " + attack_name + " for " + str(damage) + " damage"
+                    else:
+                        self.boss_health -= damage
+                        boss_action_text = self.state.story_teller.final_boss_name + " heals with " + attack_name + " for " + str(damage) + "HP"
 
-            attack_name = self.enemy_attacks[enemy_attack_index]["name"]
-            damage = int(self.enemy_attacks[enemy_attack_index]["damage"])
-            self.player_health -= damage
+                print(boss_action_text)
 
-            print("boss attacks with: " + attack_name + " for " + str(damage) + " damage")
-
-            # If health is less than 50% have a chance to use a healing attack if slot is left
-
-            # If health is less than 25% have a chance to use a healing item if not used up
-
-            # Else Pick a random attack
-
-            self.players_turn = True
-
+                self.post_action_phase = True
+                self.post_action_text = boss_action_text
+                self.players_turn = True
 
         if self.player_health <= 0:
+
             self.state.battle_won = False
             self.battle_done = True
             self.done()
